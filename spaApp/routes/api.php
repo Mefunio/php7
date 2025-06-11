d99172c2008b08f6fec6d6dd1a8d4b9c907f332b
mefunio <nemomati1@gmail.com>
2025-06-11 07:58:27 +0200

    routelist

--- spaApp/routes/api.php
+++ spaApp/routes/api.php
@@ -44,3 +44,5 @@ Route::post('/logout', function (Request $request) {
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('students', 'StudentController');
